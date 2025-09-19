10e7e80017219892cdbdaa67ff71c07936769442
brett.wolf.howells <brett.wolf.howells@gmail.com>
2025-09-18 22:59:45 -0400

    Second commit

--- bubo_db_connect.py
+++ bubo_db_connect.py
@@ -3,8 +3,11 @@ import psycopg
 from psycopg.rows import dict_row
 
 # Use DATABASE_URL from environment (Render provides this)
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://bhowells:o2NJjZoRqS8IqrZHUjAFO1ChtUraujGW@dpg-d366h83ipnbc73akr66g-a.virginia-postgres.render.com/bubodb")
+DATABASE_URL = os.environ.get("BUBO_DB_CONNECTIONSTRING")
 
 def get_connection():
     """Create a new psycopg connection to the database using URL."""
-    return psycopg.connect(DATABASE_URL, row_factory=dict_row)
+    print(DATABASE_URL)
+    #return psycopg.connect(DATABASE_URL, row_factory=dict_row)
+
+get_connection()
