5bc5107c9537affb3d399c2494668aa5803d4dd3
brett.wolf.howells <brett.wolf.howells@gmail.com>
2025-09-18 22:33:32 -0400

    First Commit

--- bubo_db_connect.py
+++ bubo_db_connect.py
@@ -6,5 +6,5 @@ from psycopg.rows import dict_row
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://bhowells:o2NJjZoRqS8IqrZHUjAFO1ChtUraujGW@dpg-d366h83ipnbc73akr66g-a.virginia-postgres.render.com/bubodb")
 
 def get_connection():
-    """Create a new psycopg connection to the database."""
+    """Create a new psycopg connection to the database using URL."""
     return psycopg.connect(DATABASE_URL, row_factory=dict_row)
